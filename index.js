3c45fe9953b5ba53b80070328c33c8fd44bf5b09
eyuleo <eyuleo10@gmail.com>
2025-03-07 20:06:27 +0300

    add CRUD operations for notes and enable JSON parsing

--- index.js
+++ index.js
@@ -11,12 +11,48 @@ let notes = [
 		important: true,
 	},
 ]
+app.use(express.json())
+
 app.get("/", (req, res) => {
 	res.send("<h1>Hello from express server!<h1/>")
 })
 
 app.get("/api/notes", (req, res) => {
 	res.json(notes)
+})
+
+app.get("/api/notes/:id", (req, res) => {
+	const id = req.params.id
+	const note = notes.find((note) => note.id === id)
+	if (note) {
+		res.json(note)
+	} else {
+		res.status(404).send({ error: "Note not found" })
+	}
+})
+
+app.post("/api/notes", (req, res) => {
+	const body = req.body
+
+	if (!body.content) {
+		return res.status(400).send({ error: "content missing" })
+	}
+
+	const note = {
+		content: body.content,
+		important: body.important || false,
+		id: crypto.randomUUID(),
+	}
+
+	notes = notes.concat(note)
+
+	res.json(note)
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+	const id = req.params.id
+	notes = notes.filter((note) => note.id !== id)
+	res.status(204).end()
 })
 
 const PORT = 8080
