f45a1a7493014727ac3c98cc7b071d5169961090
eyuleo <eyuleo10@gmail.com>
2025-04-04 20:25:57 +0300

    refactor logger to suppress output in test environment; update config for test database URI; add initial tests for notes API

---
+++ tests/note_api.test.js
@@ -0,0 +1,44 @@
+import { test, after, beforeEach } from "node:test";
+import mongoose from "mongoose";
+import supertest from "supertest";
+import app from "../app.js";
+import assert from "node:assert";
+import Note from "../models/note.js";
+
+const api = supertest(app);
+
+const initialNotes = [
+  { content: "HTML is easy", important: false },
+  { content: "Browser can execute only JavaScript", important: true },
+];
+
+beforeEach(async () => {
+  Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("there are two notes", async () => {
+  const res = await api.get("/api/notes");
+
+  assert.strictEqual(res.body.length, initialNotes.length);
+});
+test("the first note is about HTTP methods", async () => {
+  const res = await api.get("/api/notes");
+
+  const contents = res.body.map((e) => e.content);
+  assert(contents.includes("Html is easy"));
+});
+
+after(async () => {
+  mongoose.connection.close();
+});
